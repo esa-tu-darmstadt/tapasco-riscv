c0379119ac9ff3ed2a2f6fcc8309c7924616820f
tkay94 <23720108+tkay94@users.noreply.github.com>
2020-09-05 12:33:38 +0200

    Add __iob implementation
    Add implementation of putc and getc operations on stdin/stdout. In initialization phase base addresses of the RAM buffers are fetched. One buffer is used to write stdout output and one for "reading" from stdin.

--- picolibc_support/tapasco_iob/iob.c
+++ picolibc_support/tapasco_iob/iob.c
@@ -31,39 +31,81 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
+ *
+ * Copyright © 2020 Embedded Systems and Applications Group, TU Darmstadt
+ * 
+ * Modified underlaying source code for own usage.
+ * 
  */
 
 #include <stdio.h>
 #include <sys/cdefs.h>
 
+#define STDOUT_BASE ((int *) 0x11000080)
+#define STDOUT_SIZE ((int *) 0x11000090)
+#define STDIN_BASE  ((int *) 0x110000A0)
+#define STDIN_SIZE  ((int *) 0x110000B0)
+#define RAM_OFFSET  0x80000000
+
 /*
- * Dummy stdio hooks. This allows programs to link without requiring
- * any system-dependent functions. This is only used if the program
- * doesn't provide its own version of __iob
+ * stdio implementation for RISC-V cores included in TaPaSCo
  */
 
-static int
-dummy_putc(char c, FILE *file)
+char * stdout_buffer = 0;
+int stdout_offset = 0;
+int stdout_size = 0;
+
+char * stdin_buffer = 0;
+int stdin_offset = 0;
+int stdin_size = 0;
+
+/* 
+ * initialize pointers before main() is even called
+ */
+static void __attribute__((constructor)) init_iob()
 {
-	(void) c;
-	(void) file;
-	return c;
+	stdout_buffer = (char *) (*STDOUT_BASE + RAM_OFFSET);
+	stdout_size = *STDOUT_SIZE;
+	stdin_buffer = (char *) (*STDIN_BASE + RAM_OFFSET);
+	stdin_size = *STDIN_SIZE;
 }
 
 static int
-dummy_getc(FILE *file)
+tapasco_putc(char c, FILE *file)
 {
-	(void) file;
+	// check whether buffer address is set and size is not exceeded
+	if (stdout_buffer != 0 && stdout_offset < stdout_size) {
+		stdout_buffer[stdout_offset] = c;
+		++stdout_offset;
+		return c;
+	}
 	return EOF;
 }
 
 static int
-dummy_flush(FILE *file)
+tapasco_getc(FILE *file)
+{
+	// check whether buffer address is set and size is not exceeded
+	if (stdin_buffer != 0 && stdin_offset < stdin_size) {
+		char c = stdin_buffer[stdin_offset];
+		++stdin_offset;
+		return c;
+	}
+	return EOF;
+}
+
+/*
+ * flush is not implemented since all data is written directly to memory anyway
+ */ 
+static int
+tapasco_flush(FILE *file)
 {
 	(void) file;
 	return 0;
 }
 
-static FILE __stdio = FDEV_SETUP_STREAM(dummy_putc, dummy_getc, dummy_flush, _FDEV_SETUP_RW);
+static FILE __stdio = FDEV_SETUP_STREAM(tapasco_putc, tapasco_getc, tapasco_flush, _FDEV_SETUP_RW);
 
 FILE *const __iob[3] = { &__stdio, &__stdio, &__stdio };
