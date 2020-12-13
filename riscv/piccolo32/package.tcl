3664557e1c427c3ef4334aa2e124c304ef8c8a05
Kai Meinhard <kaimeinhard@hotmail.de>
2020-12-13 11:29:36 +0100

    Adds support for DMI port in Piccolo

--- riscv/piccolo32/package.tcl
+++ riscv/piccolo32/package.tcl
@@ -2,16 +2,28 @@ set name "RV64ACIMU"
 if {$::argc > 0} {
     set name [string trim [lindex $::argv 0]]
 }
+set jtag_version 0
+if {$::argc > 1} {
+    set jtag_version [string trim [lindex $::argv 1]]
+}
 puts "package piccolo $name"
 
 set version 0.1
 create_project -force $name
-add_files [glob Piccolo/builds/${name}_Piccolo_verilator/Verilog_RTL/*.v]
+if {$jtag_version == 1} {
+    add_files [glob Piccolo/src_SSITH_P1/xilinx_ip/hdl/*.v]
+} else {
+    add_files [glob Piccolo/builds/${name}_Piccolo_verilator/Verilog_RTL/*.v]
+}
 add_files [glob Piccolo/src_bsc_lib_RTL/*.v]
 remove_files  Piccolo/src_bsc_lib_RTL/main.v
 update_compile_order -fileset sources_1
 
-set_property top mkCore [current_fileset]
+if {$jtag_version == 1} {
+    set_property top mkP1_Core [current_fileset]
+} else {
+    set_property top mkCore [current_fileset]
+}
 update_compile_order -fileset sources_1
 
 remove_files [get_files -filter {IS_AUTO_DISABLED}]
@@ -26,32 +38,41 @@ set_property description $name $core
 set_property version $version $core
 set_property core_revision 1 $core
 
-# Get IP definition of DMI
-set tapasco_toolflow $::env(TAPASCO_HOME_TOOLFLOW)
-set_property ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $tapasco_toolflow/vivado/common/ip/DMI/] [current_project]
-update_ip_catalog
+if {$jtag_version == 1} {
+    ipx::infer_bus_interfaces xilinx.com:interface:jtag_rtl:2.0 [ipx::current_core]
 
-# Map DMI interface
-ipx::add_bus_interface DMI [ipx::current_core]
-set_property abstraction_type_vlnv esa.informatik.tu-darmstadt.de:user:DMI_rtl:1.0 [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property bus_type_vlnv esa.informatik.tu-darmstadt.de:user:DMI:1.0 [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-ipx::add_port_map REQ_W_ADDRESS [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name dm_dmi_write_dm_addr [ipx::get_port_maps REQ_W_ADDRESS -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
-ipx::add_port_map REQ_READ [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name EN_dm_dmi_read_data [ipx::get_port_maps REQ_READ -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
-ipx::add_port_map RSP_DATA [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name dm_dmi_read_data [ipx::get_port_maps RSP_DATA -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
-ipx::add_port_map REQ_DATA [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name dm_dmi_write_dm_word [ipx::get_port_maps REQ_DATA -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
-ipx::add_port_map REQ_WRITE [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name EN_dm_dmi_write [ipx::get_port_maps REQ_WRITE -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
-ipx::add_port_map REQ_ADDRESS [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name dm_dmi_read_addr_dm_addr [ipx::get_port_maps REQ_ADDRESS -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    # TCK is not mapped properly
+    ipx::add_port_map TCK [ipx::get_bus_interfaces jtag -of_objects [ipx::current_core]]
+    set_property physical_name jtag_tclk [ipx::get_port_maps TCK -of_objects [ipx::get_bus_interfaces jtag -of_objects [ipx::current_core]]]
+} else {
+    # Get IP definition of DMI
+    set tapasco_toolflow $::env(TAPASCO_HOME_TOOLFLOW)
+    set_property ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $tapasco_toolflow/vivado/common/ip/DMI/] [current_project]
+    update_ip_catalog
+
+    # Map DMI interface
+    ipx::add_bus_interface DMI [ipx::current_core]
+    set_property abstraction_type_vlnv esa.informatik.tu-darmstadt.de:user:DMI_rtl:1.0 [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property bus_type_vlnv esa.informatik.tu-darmstadt.de:user:DMI:1.0 [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    ipx::add_port_map REQ_W_ADDRESS [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name dm_dmi_write_dm_addr [ipx::get_port_maps REQ_W_ADDRESS -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    ipx::add_port_map REQ_READ [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name EN_dm_dmi_read_data [ipx::get_port_maps REQ_READ -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    ipx::add_port_map RSP_DATA [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name dm_dmi_read_data [ipx::get_port_maps RSP_DATA -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    ipx::add_port_map REQ_DATA [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name dm_dmi_write_dm_word [ipx::get_port_maps REQ_DATA -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    ipx::add_port_map REQ_WRITE [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name EN_dm_dmi_write [ipx::get_port_maps REQ_WRITE -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    ipx::add_port_map REQ_ADDRESS [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
+    set_property physical_name dm_dmi_read_addr_dm_addr [ipx::get_port_maps REQ_ADDRESS -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
 ipx::add_port_map REQ_VALID [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]
-set_property physical_name EN_dm_dmi_read_addr [ipx::get_port_maps REQ_VALID -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
+    set_property physical_name EN_dm_dmi_read_addr [ipx::get_port_maps REQ_VALID -of_objects [ipx::get_bus_interfaces DMI -of_objects [ipx::current_core]]]
 
 
-set_property driver_value 1 [ipx::get_ports cpu_reset_server_request_put -of_objects [ipx::current_core]]
+    set_property driver_value 1 [ipx::get_ports cpu_reset_server_request_put -of_objects [ipx::current_core]]
+}
+
 
 ipx::create_xgui_files $core
 ipx::update_checksums $core
--- specific_tcl/piccolo32_pe_project.tcl
+++ specific_tcl/piccolo32_pe_project.tcl
@@ -1,23 +1,35 @@
 # Create instance: piccolo_0, and set properties
   set piccolo_0 [ create_bd_cell -type ip -vlnv [dict get $cpu_vlnv $project_name] piccolo_0 ]
   set cpu_clk [get_bd_pins piccolo_0/CLK]
+
+  set jtag_version 0
+  if {$jtag_version == 1} {
+    set cpu_dmem_master master0
+    set cpu_imem_master master1
+  } else {
+    set cpu_dmem_master cpu_dmem_master
+    set cpu_imem_master cpu_imem_master
+  }
+
+  if 0 {
   set_property -dict [ list \
    CONFIG.SUPPORTS_NARROW_BURST {0} \
    CONFIG.NUM_READ_OUTSTANDING {1} \
    CONFIG.NUM_WRITE_OUTSTANDING {1} \
    CONFIG.MAX_BURST_LENGTH {1} \
- ] [get_bd_intf_pins /piccolo_0/cpu_dmem_master]
+ ] [get_bd_intf_pins /piccolo_0/$cpu_dmem_master]
+ }
 
   set_property -dict [ list \
    CONFIG.SUPPORTS_NARROW_BURST {0} \
    CONFIG.NUM_READ_OUTSTANDING {1} \
    CONFIG.NUM_WRITE_OUTSTANDING {1} \
    CONFIG.MAX_BURST_LENGTH {1} \
- ] [get_bd_intf_pins /piccolo_0/cpu_imem_master]
+ ] [get_bd_intf_pins /piccolo_0/$cpu_imem_master]
 
   # Create interface connections
-  connect_bd_intf_net -intf_net piccolo_0_cpu_dmem_master [get_bd_intf_pins axi_mem_intercon_1/S00_AXI] [get_bd_intf_pins piccolo_0/cpu_dmem_master]
-  set iaxi [get_bd_intf_pins piccolo_0/cpu_imem_master]
+  connect_bd_intf_net -intf_net piccolo_0_cpu_dmem_master [get_bd_intf_pins axi_mem_intercon_1/S00_AXI] [get_bd_intf_pins piccolo_0/$cpu_dmem_master]
+  set iaxi [get_bd_intf_pins piccolo_0/$cpu_imem_master]
   
   # Create port connections
   connect_bd_net -net RVController_0_reqEN [get_bd_pins RVController_0/reqEN] [get_bd_pins piccolo_0/EN_cpu_reset_server_request_put]
@@ -26,20 +38,38 @@
   connect_bd_net -net piccolo_0_RDY_cpu_reset_server_request_put [get_bd_pins RVController_0/reqRDY_req_rdy] [get_bd_pins piccolo_0/RDY_cpu_reset_server_request_put]
   connect_bd_net -net piccolo_0_RDY_cpu_reset_server_response_get [get_bd_pins RVController_0/resRDY_res_rdy] [get_bd_pins piccolo_0/RDY_cpu_reset_server_response_get]
 
-  # Get IP definition of DMI
-  set tapasco_toolflow $::env(TAPASCO_HOME_TOOLFLOW)
-  set_property ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $tapasco_toolflow/vivado/common/ip/DMI/] [current_project]
-  update_ip_catalog
+  if {$jtag_version == 1} {
+    # Connect JTAG interface to the outside
+    create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:jtag_rtl:2.0 JTAG
+    connect_bd_intf_net [get_bd_intf_ports JTAG] [get_bd_intf_pins piccolo_0/jtag]
 
-  # Connect DMI port to the outside
-  create_bd_intf_port -mode Slave -vlnv esa.informatik.tu-darmstadt.de:user:DMI_rtl:1.0 DMI
-  connect_bd_intf_net [get_bd_intf_ports DMI] [get_bd_intf_pins piccolo_0/DMI]
+    create_bd_port -dir O -type clk RTCK
+    connect_bd_net [get_bd_ports RTCK] [get_bd_pins piccolo_0/CLK_jtag_tclk_out]
+  } else {
+    # Get IP definition of DMI
+    set tapasco_toolflow $::env(TAPASCO_HOME_TOOLFLOW)
+    set_property ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $tapasco_toolflow/vivado/common/ip/DMI/] [current_project]
+    update_ip_catalog
+
+    # Connect DMI port to the outside
+    create_bd_intf_port -mode Slave -vlnv esa.informatik.tu-darmstadt.de:user:DMI_rtl:1.0 DMI
+    connect_bd_intf_net [get_bd_intf_ports DMI] [get_bd_intf_pins piccolo_0/DMI]
+  }
 
 proc create_specific_addr_segs {} {
   variable lmem
+  set jtag_version 0
+  if {$jtag_version == 1} {
+    set cpu_dmem_master master0
+    set cpu_imem_master master1
+  } else {
+    set cpu_dmem_master cpu_dmem_master
+    set cpu_imem_master cpu_imem_master
+  }
+ 
   # Create specific address segments
-  create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
-  create_bd_addr_seg -range 0x80000000 -offset 0x80000000 [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs dmaOffset/S_AXI/reg0] SEG_dmaOffset_reg0
-  create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
-  create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces piccolo_0/cpu_imem_master] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
+  create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces piccolo_0/$cpu_dmem_master] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
+  create_bd_addr_seg -range 0x80000000 -offset 0x80000000 [get_bd_addr_spaces piccolo_0/$cpu_dmem_master] [get_bd_addr_segs dmaOffset/S_AXI/reg0] SEG_dmaOffset_reg0
+  create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces piccolo_0/$cpu_dmem_master] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
+  create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces piccolo_0/$cpu_imem_master] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
 }
