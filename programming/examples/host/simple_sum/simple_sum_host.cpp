85c96a1002f68395b93569c0f5965f476e0c6d5b
7FM <41307817+7FM@users.noreply.github.com>
2021-04-29 13:27:52 +0200

    allow specifying PE_ID over args

--- programming/examples/host/simple_sum/simple_sum_host.cpp
+++ programming/examples/host/simple_sum/simple_sum_host.cpp
@@ -1,15 +1,15 @@
-#include <fstream>
-#include <memory>
-#include <iterator>
-#include <vector>
-#include <iostream>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "tapasco.hpp"
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
 
 #define PE_ID 1748
 
@@ -23,21 +23,25 @@ void read_binary_file(std::string filename, std::vector<char> &buffer) {
     int fd = open(filename.c_str(), O_RDONLY);
     struct stat sb;
     fstat(fd, &sb);
-    char *buf = (char*)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    char *buf = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
     buffer.insert(buffer.end(), buf, buf + sb.st_size);
     munmap(buf, sb.st_size);
     close(fd);
     std::cout << "Finished reading binary file. Received " << buffer.size() << " bytes." << std::endl;
 }
 
-int main(int argc, char** argv)
-{
-    if(argc < 2)
-    {
+int main(int argc, char **argv) {
+    if (argc < 2) {
         printf("Missing program code, exiting...");
         exit(1);
     }
-    
+
+    int peID = PE_ID;
+
+    if (argc > 2) {
+        peID = atoi(argv[2]);
+    }
+
     Tapasco tapasco;
 
     std::vector<char> program_buffer;
@@ -54,19 +58,23 @@ int main(int argc, char** argv)
     // Wrap the program buffer into local memory object
     auto program_buffer_in = makeLocal(makeInOnly(
             makeWrappedPointer(program_buffer.data(), program_buffer.size())
-        ));
+    ));
 
     uint64_t fpga_sum = -1;
     RetVal<uint64_t> retval(&fpga_sum);
+
     int a = 42;
     int b = 1337;
-    auto job = tapasco.launch(PE_ID,
-                                retval, // return value
-                                program_buffer_in,
-                                a, // Arg 1
-                                b, // Arg 2
-                                makeOutOnly(makeWrappedPointer(stdoutBuf, STDOUT_BUF)) // Arg3
-                             );
+
+    auto job = tapasco.launch(
+        peID,                                                  // Processing Element ID
+        retval,                                                // return value
+        program_buffer_in,                                     // Program is passed as Arg 0
+        a,                                                     // Arg 1
+        b,                                                     // Arg 2
+        makeOutOnly(makeWrappedPointer(stdoutBuf, STDOUT_BUF)) // Arg 3
+    );
+
     cout << "Waiting for RISC-V " << endl;
     job();
     cout << "RISC-V return value: " << (fpga_sum & 0x0FFFFFFFF) << endl;
