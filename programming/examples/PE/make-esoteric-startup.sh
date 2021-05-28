b381ddb660d7396ede9f2778d9e80940a5ccd594
Jörn Schöndube <js45vybi@eredwithrim.esa.informatik.tu-darmstadt.de>
2021-05-28 20:17:16 +0200

    add esoteric program that is easy to trace in gtkwave

---
+++ programming/examples/PE/make-esoteric-startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+N=$1
+
+BYTES="0"
+
+for i in $(seq $((N - 1)));
+do
+	BYTES="$BYTES, $i"
+done
+
+echo $BYTES
+
+cat > $2 << EOF
+.section ".text.init"
+.globl _start
+_start:
+        .word $BYTES
+init_stack:
+	lui sp, 0x00488
+	addi sp, sp, 0x000
+	jal main
+EOF
+
