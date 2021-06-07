c72d6cb162532645a96b30e19ed4f9be7a198b7f
7FM <41307817+7FM@users.noreply.github.com>
2021-06-07 16:45:22 +0200

    heavy copy & paste of scr1 setup, needs adjusting

---
+++ debugging/logdbg/cva6_sim/Makefile_verilator.mk
@@ -0,0 +1,81 @@
+ifndef CAPN_PATH
+	$(error CAPN_PATH is undefined)
+endif
+
+SRC ?= cva6/src
+SRC_FILES ?= ../../../riscv/cva6/core.files
+ADD_SCRS ?= 
+INCLUDE_DIR ?= cva6/include
+SIM_SRC ?= sim_src
+V_DIR ?= sim_build
+
+SIM_DEFS ?= -DRUN_SIM
+
+DEFAULT_TOP_MODULE := ariane_custom_tb_top
+
+VERILATOR_PREFIX ?=
+VERILATOR := $(VERILATOR_PREFIX)verilator
+
+rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(subst *,%,$2),$d))
+
+srcs += $(ADD_SCRS)
+#srcs += $(call rwildcard,$(SRC),*.v)
+#srcs += $(call rwildcard,$(SRC),*.sv)
+
+#sim_srcs := $(call rwildcard,$(SIM_SRC),*.cpp)
+sim_targets := $(wildcard $(SIM_SRC)/sim_*.cpp)
+sim_tops := $(patsubst sim_%.cpp,%,$(notdir $(sim_targets)))
+
+deps := $(call rwildcard,$(V_DIR),*.d)
+
+PWD := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+V_CCFLAGS := -g -Wall -O3 -I$(PWD)$(SIM_SRC) -I$(CAPN_PATH)/lib
+V_LDFLAGS := -lpthread
+
+.PHONY: all clean sim preparebuild cva6
+
+all: preparebuild cva6 sim
+
+preparebuild:
+#	cp dm_interface.* $(SIM_SRC)/$(DEFAULT_TOP_MODULE)
+#	cp dm_testbench_interface.* $(SIM_SRC)/$(DEFAULT_TOP_MODULE)
+#	cp tapasco-riscv.capnp.* $(SIM_SRC)/$(DEFAULT_TOP_MODULE)
+
+cva6:
+	git clone https://github.com/jschj/cva6 && cd cva6 && git checkout trials || cd cva6 && git pull
+
+define GEN_SIM_RULES
+.PHONY: sim_$(sim_top)
+
+sim_$(sim_top): $$(V_DIR)/$(sim_top)/Vsim_$(sim_top).mk $$(SIM_SRC)/sim_$(sim_top).cpp $$(call rwildcard,$$(SIM_SRC)/$(sim_top),*.cpp) $$(call rwildcard,$$(SIM_SRC)/$(sim_top),*.c) $(CAPN_PATH)/lib/*.o $$(srcs)
+	@make -C $$(V_DIR)/$(sim_top) -f V$(sim_top).mk V$(sim_top)
+	@echo "============================================================================================"
+
+$$(V_DIR)/$(sim_top)/Vsim_$(sim_top).mk: $$(call rwildcard,$$(SIM_SRC)/$(sim_top),*.cpp) $$(call rwildcard,$$(SIM_SRC)/$(sim_top),*.c) $(CAPN_PATH)/lib/*.o $$(srcs)
+	@mkdir -p $$(V_DIR)/$(sim_top)
+	@mkdir -p $$(V_DIR)/$(sim_top)/$$(SIM_SRC)
+	$$(VERILATOR) \
+		-Wall -Werror-IMPLICIT -Werror-PINMISSING \
+		--MMD --MP $$(SIM_DEFS) \
+	 	--Mdir $$(V_DIR)/$(sim_top) --cc -O3 \
+		-CFLAGS "$$(V_CCFLAGS) -I$$(PWD)$$(SIM_SRC)/$(sim_top)" \
+	 	-LDFLAGS "$$(V_LDFLAGS)" \
+		-I$$(SRC) -I$$(INCLUDE_DIR) \
+		--exe $$(PWD)$$(SIM_SRC)/sim_$(sim_top).cpp $$(call rwildcard,$$(PWD)$$(SIM_SRC)/$(sim_top),*.cpp) $$(call rwildcard,$$(PWD)$$(SIM_SRC)/$(sim_top),*.c) $(CAPN_PATH)/lib/*.o \
+		-sv --top-module $(sim_top) --trace --trace-structs $$(srcs) || true
+	@echo "============================================================================================"
+endef
+
+$(foreach sim_top,$(sim_tops), \
+	$(eval $(GEN_SIM_RULES)) \
+)
+
+sim: sim_$(DEFAULT_TOP_MODULE)
+
+clean:
+	rm -rf $(V_DIR)
+	rm -f $(SIM_SRC)/$(DEFAULT_TOP_MODULE)/dm_interface.*
+	rm -f $(SIM_SRC)/$(DEFAULT_TOP_MODULE)/dm_testbench_interface.*
+	rm -f $(SIM_SRC)/$(DEFAULT_TOP_MODULE)/tapasco-riscv.capnp.*
+
+-include $(deps)
---
+++ debugging/logdbg/cva6_sim/sim_src/sim_ariane_custom_tb_top.cpp
@@ -0,0 +1,201 @@
+#include <bitset>
+#include <csignal>
+#include <cstdint>
+#include <getopt.h>
+#include <iostream>
+#include <verilated.h> // Defines common routines
+#include <verilated_vcd_c.h>
+
+#define TOP_MODULE Variane_custom_tb_top
+
+#include "Variane_custom_tb_top.h"       // basic Top header
+#include "Variane_custom_tb_top__Syms.h" // all headers to access exposed internal signals
+
+#include "DMI_Handler.hpp"
+#include "dm_testbench_interface.hpp"
+
+#ifndef PHASE_LENGTH
+#define PHASE_LENGTH 5
+#endif
+
+static TOP_MODULE *ptop = nullptr; // Instantiation of module
+static VerilatedVcdC *tfp = nullptr;
+
+static v2dmi::DMI_Handler *dmiHandler = nullptr;
+
+static vluint64_t main_time = 0; // Current simulation time
+
+static volatile bool runSim = true;
+
+static void signal_handler(int signum) {
+    if (signum == SIGINT) {
+        std::cout << "Trying to exit..." << std::endl;
+        runSim = false;
+    }
+}
+
+static bool stopCondition();
+static void sanityChecks();
+static void onRisingEdge();
+static void onFallingEdge();
+
+/**
+* Called by $time in Verilog
+****************************************************************************/
+double sc_time_stamp() {
+    return main_time; // converts to double, to match what SystemC does
+}
+
+/******************************************************************************/
+static void tick(int count, bool dump) {
+    do {
+        //if (tfp)
+        //tfp->dump(main_time); // dump traces (inputs stable before outputs change)
+        ptop->eval(); // Evaluate model
+        main_time++;  // Time passes...
+        if (tfp && dump)
+            tfp->dump(main_time); // inputs and outputs all updated at same time
+    } while (--count);
+}
+
+/******************************************************************************/
+
+static void run(uint64_t limit, bool dump, bool checkStopCondition = true) {
+    bool stop;
+    do {
+        stop = checkStopCondition && stopCondition();
+        ptop->clk = 1;
+        onRisingEdge();
+        tick(PHASE_LENGTH, dump);
+
+        ptop->clk = 0;
+        onFallingEdge();
+        tick(PHASE_LENGTH, dump);
+        sanityChecks();
+    } while (--limit && !stop);
+}
+
+/******************************************************************************/
+static void reset(const std::shared_ptr<dm::DM_TestBenchInterface>& dm_interface) {
+    // Initialize signals & perform reset
+    ptop->dmi_req = 0;
+    ptop->dmi_wr = 0;
+    ptop->dmi_addr = 0;
+    ptop->dmi_wdata = 0;
+
+    ptop->rst_n = 0;
+    run(100, false);
+    ptop->rst_n = 1;
+
+    if (dmiHandler) {
+        delete dmiHandler;
+    }
+    dmiHandler = new v2dmi::DMI_Handler(dm_interface);
+}
+
+/******************************************************************************/
+
+static void sanityChecks() {
+}
+
+static bool stopCondition() {
+    // Finish when $finish() was called
+    return !runSim || Verilated::gotFinish();
+}
+
+static void onRisingEdge() {
+}
+
+static void onFallingEdge() {
+    if (dmiHandler) {
+        dmiHandler->tick(ptop, runSim);
+    }
+}
+
+/******************************************************************************/
+int main(int argc, char **argv) {
+    // Register signal handler in order to properly terminate the simulation
+    std::signal(SIGINT, signal_handler);
+
+    Verilated::commandArgs(argc, argv);
+    ptop = new TOP_MODULE; // Create instance
+
+    std::shared_ptr<dm::RequestResponseFIFO> rr_fifo = std::make_shared<dm::RequestResponseFIFO>();
+    std::shared_ptr<dm::DM_TestBenchInterface> dm_interface = std::make_shared<dm::DM_TestBenchInterface>(rr_fifo);
+    dm::OpenOCDServer *server = nullptr;
+
+    int verbose = 0;
+    int start = 0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, ":s:t:v:o")) != -1) {
+        switch (opt) {
+            case 'v':
+                verbose = std::atoi(optarg);
+                break;
+            case 't':
+                // init trace dump
+                Verilated::traceEverOn(true);
+                tfp = new VerilatedVcdC;
+                ptop->trace(tfp, 99);
+                tfp->open(optarg);
+                break;
+            case 's':
+                start = std::atoi(optarg);
+                break;
+            case ':':
+                std::cout << "option needs a value" << std::endl;
+                goto exitAndCleanup;
+                break;
+            case 'o':
+                // Run simulation with an openocd connection
+                server = new dm::OpenOCDServer("/tmp/riscv-debug.sock", rr_fifo);
+                break;
+            case '?': //used for some unknown options
+                std::cout << "unknown option: " << optopt << std::endl;
+                goto exitAndCleanup;
+                break;
+        }
+    }
+
+    // start things going
+    reset(dm_interface);
+
+    if (start) {
+        run(start, false);
+    }
+
+    if (server) {
+        server->start_listening();
+    }
+
+    // Execute till stop condition
+    do {
+        run(0, true);
+    } while (!stopCondition());
+
+    if (server) {
+        server->stop_listening();
+    }
+
+exitAndCleanup:
+    if (server) {
+        delete server;
+    }
+
+    if (dmiHandler) {
+        delete dmiHandler;
+    }
+
+    if (tfp)
+        tfp->close();
+
+    ptop->final(); // Done simulating
+
+    if (tfp)
+        delete tfp;
+
+    delete ptop;
+
+    return 0;
+}
