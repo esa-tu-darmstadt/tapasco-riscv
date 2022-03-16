34e78bd6f87a2b46d44504f31383813976e9eb32
Carsten Heinz <heinz@esa.tu-darmstadt.de>
2022-03-16 17:11:14 +0100

    Update SweRV to current version from CHIPS Alliance

--- riscv/swerv/setup.sh
+++ riscv/swerv/setup.sh
@@ -2,12 +2,12 @@
 
 mkdir -p IP/riscv/
 cd riscv/swerv
-git clone https://github.com/westerndigitalcorporation/swerv_eh1.git
+git clone https://github.com/chipsalliance/Cores-SweRV.git swerv_eh1
 cd swerv_eh1
 git apply ../swerv_tapasco.patch
 export RV_ROOT=$(pwd)
 cd configs
-./swerv.config -set reset_vec=0x0 -set dccm_enable=1 -set iccm_enable=1 -set icache_enable=1
+./swerv.config -unset=assert_on -set=fpga_optimize=1 -set reset_vec=0x0 -set dccm_enable=1 -set iccm_enable=1 -set icache_enable=1
 cd ../..
 vivado -nolog -nojournal -mode batch -source package.tcl
 
