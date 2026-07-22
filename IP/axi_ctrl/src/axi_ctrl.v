8809c447ff62e3b4bb62e140a7e43a50a614ffcb
Florian Meisel <meisel@esa.tu-darmstadt.de>
2026-07-22 19:10:25 +0200

    Add read&write burst support to axi_ctrl
    Read bursts were somewhat supported before, but had several bugs

--- IP/axi_ctrl/src/axi_ctrl.v
+++ IP/axi_ctrl/src/axi_ctrl.v
@@ -170,13 +170,18 @@ if (AXI_PROTOCOL == "AXI4LITE") begin
     end
 end else begin // AXI4 FULL
 
+    wire awready;
+    wire wready;
+    wire arready;
     reg[8:0] arlen_reg;
     reg [ADDRESS_WIDTH-1:0] araddr_reg, awaddr_reg;
     reg [ID_WIDTH-1:0] rid, wid;
     
     assign bram_clk = CLK;
     assign bram_rst = !RST_N;
-    assign S_AXI_arready = 1;
+    assign S_AXI_awready = awready;
+    assign S_AXI_wready = wready;
+    assign S_AXI_arready = arready;
     assign S_AXI_rvalid = (arlen_reg > 0);
     assign S_AXI_rdata = bram_dout;
     assign S_AXI_rlast = (arlen_reg == 1);
@@ -188,10 +193,10 @@ end else begin // AXI4 FULL
             araddr_reg <= 0;
             rid <= 0;
         end else begin
-            rid <= S_AXI_arvalid ? S_AXI_arid : rid;
-            arlen_reg <= S_AXI_arvalid ? (S_AXI_arlen + 1) : 
+            rid <= (S_AXI_arvalid && arready) ? S_AXI_arid : rid;
+            arlen_reg <= (S_AXI_arvalid && arready) ? (S_AXI_arlen + 1) :
                             ((arlen_reg > 0) ? arlen_reg - 1 : 0);
-            araddr_reg <= S_AXI_arvalid ? (S_AXI_araddr + BYTES_PER_WORD) : (araddr_reg + BYTES_PER_WORD);
+            araddr_reg <= (S_AXI_arvalid && arready) ? (S_AXI_araddr + BYTES_PER_WORD) : (araddr_reg + BYTES_PER_WORD);
         end
     end
 
@@ -208,52 +213,99 @@ end else begin // AXI4 FULL
     end
 
     if(READ_ONLY == 1) begin
+        assign arready = (arlen_reg <= 1);
+        assign awready = 0;
+        assign wready = 0;
         assign bram_addr = S_AXI_arvalid ? S_AXI_araddr : araddr_reg;
         assign bram_en = S_AXI_arvalid | (arlen_reg > 1);
         assign bram_we = 0;
         assign bram_din = 0;
-        assign S_AXI_awready = 0;
-        assign S_AXI_wready = 0;
         assign S_AXI_bid = 0;
         assign S_AXI_bresp = 0;
         assign S_AXI_bvalid = 0;
     end else begin
         reg wlast_reg;
-        reg [1:0] wen_reg;
+        reg write_data_before_address_reg;
+        reg write_burst_pending_reg;
+        reg [BYTES_PER_WORD-1:0] wstrb_reg;
         reg [BYTES_PER_WORD*8-1:0] wdata_reg;
-        assign bram_addr = (S_AXI_wvalid) ? (S_AXI_awvalid ? S_AXI_awaddr : awaddr_reg) : 
-                                (S_AXI_arvalid ? S_AXI_araddr : araddr_reg);
-        assign bram_en = S_AXI_arvalid | S_AXI_wvalid;
-        assign bram_din = S_AXI_wvalid ? S_AXI_wdata : wdata_reg;
-        assign bram_we = ((S_AXI_awvalid && S_AXI_wvalid) || (wen_reg[0] && S_AXI_wvalid) || (wen_reg[1] && S_AXI_awvalid)) ? S_AXI_wstrb : 0;
-        assign S_AXI_awready = 1;
-        assign S_AXI_wready = 1;
+        assign awready = (arlen_reg <= 1) && !write_burst_pending_reg;
+        assign wready = !write_data_before_address_reg && (arlen_reg == 0) && !wlast_reg;
+        assign arready = !write_burst_pending_reg && (arlen_reg <= 1);
+        reg [ADDRESS_WIDTH-1:0] bram_addr_comb;
+        always @(*) begin
+            bram_addr_comb = araddr_reg;
+            if (S_AXI_wvalid && wready || write_data_before_address_reg && S_AXI_awvalid && awready) begin
+                bram_addr_comb = (S_AXI_awvalid && awready) ? S_AXI_awaddr : awaddr_reg;
+            end
+            else if (S_AXI_arvalid && arready) begin
+                bram_addr_comb = S_AXI_araddr;
+            end
+            else if (arlen_reg > 1) begin
+                bram_addr_comb = araddr_reg; //redundant with default assign
+            end
+        end
+        reg bram_en_comb;
+        reg [BYTES_PER_WORD-1:0] bram_we_comb;
+        always @(*) begin
+            bram_en_comb = 0;
+            bram_we_comb = 0;
+            if ((write_data_before_address_reg || S_AXI_wvalid && wready) && (S_AXI_awvalid && awready || write_burst_pending_reg)) begin
+                bram_en_comb = 1;
+                bram_we_comb = write_data_before_address_reg ? wstrb_reg : S_AXI_wstrb;
+            end
+            else if (S_AXI_arvalid && arready || arlen_reg > 1) begin
+                bram_en_comb = 1; //read
+            end
+        end
+        assign bram_addr = bram_addr_comb;
+        assign bram_en = bram_en_comb;
+        assign bram_din = (S_AXI_wvalid && !write_data_before_address_reg) ? S_AXI_wdata : wdata_reg;
+        assign bram_we = bram_we_comb;
         assign S_AXI_bresp = 0;
         assign S_AXI_bvalid = wlast_reg;
         assign S_AXI_bid = wid;
 
         always @(posedge CLK) begin
             if (!RST_N) begin
+                awaddr_reg <= 0;
+                write_burst_pending_reg <= 0;
                 wlast_reg <= 0;
                 wid = 0;
+                wstrb_reg <= 0;
                 wdata_reg <= 0;
-                wen_reg <= 0;
+                write_data_before_address_reg <= 0;
             end else begin
-                wlast_reg <= (wlast_reg && !S_AXI_bready) ? 1 : S_AXI_wlast && S_AXI_wvalid;
-                wid <= S_AXI_awvalid ? S_AXI_awid : wid;
-                awaddr_reg <= S_AXI_awvalid ? S_AXI_awaddr : awaddr_reg;
-                wdata_reg <= S_AXI_wvalid ? S_AXI_wdata : wdata_reg;
-                if (S_AXI_wvalid && !S_AXI_awvalid) begin
-                    // data before address
-                    wen_reg[1] <= 1;
-                end else if (wen_reg[1] && S_AXI_awvalid) begin
-                    wen_reg[1] <= 0;
+                wlast_reg <= (wlast_reg && !S_AXI_bready) ? 1 : (S_AXI_wlast && S_AXI_wvalid && wready);
+
+                if (S_AXI_wvalid && wready) begin
+                    if (write_burst_pending_reg)
+                        write_burst_pending_reg <= !S_AXI_wlast;
+                    wdata_reg <= S_AXI_wdata;
+                    wstrb_reg <= S_AXI_wstrb;
+                    awaddr_reg <= awaddr_reg + BYTES_PER_WORD;
                 end
-                if (S_AXI_awvalid && !S_AXI_wvalid) begin
-                    // addr before address
-                    wen_reg[0] <= 1;
-                end else if (wen_reg[0] && S_AXI_wvalid) begin
-                    wen_reg[0] <= 0;
+                if (S_AXI_awvalid && awready) begin
+                    wid <= S_AXI_awid;
+                    //Ignore S_AXI_awlen, just use S_AXI_wlast to trigger the last beat
+                    if (write_data_before_address_reg) begin
+                        write_burst_pending_reg <= !wlast_reg;
+                        awaddr_reg <= S_AXI_awaddr + BYTES_PER_WORD;
+                    end
+                    else if (S_AXI_wvalid && wready) begin
+                        write_burst_pending_reg <= !S_AXI_wlast;
+                        awaddr_reg <= S_AXI_awaddr + BYTES_PER_WORD;
+                    end
+                    else begin
+                        write_burst_pending_reg <= 1;
+                        awaddr_reg <= S_AXI_awaddr;
+                    end
+                end
+                if ((S_AXI_wvalid && wready) && !(S_AXI_awvalid && awready) && !write_burst_pending_reg) begin
+                    // data before address
+                    write_data_before_address_reg <= 1;
+                end else if (write_data_before_address_reg && S_AXI_awvalid && awready) begin
+                    write_data_before_address_reg <= 0;
                 end
             end
         end
@@ -274,35 +326,52 @@ end else begin // AXI4 FULL
                     $display("AXI full master never read and write in same cycle.");
                     $finish;
                 end
-                if (S_AXI_awvalid && S_AXI_awlen != 0) begin
+                if (S_AXI_arvalid && S_AXI_arlen != 0 && S_AXI_arburst != 2'b01) begin
                     status[2] <= 1;
                     // This is a simplification
-                    $display("We currently do not support write bursts.");
+                    $display("We only support INCR read bursts.");
                     $finish;
                 end
-                if (S_AXI_awvalid && arlen_reg != 0) begin
+                if (S_AXI_awvalid && S_AXI_awlen != 0 && S_AXI_awburst != 2'b01) begin
                     status[3] <= 1;
                     // This is a simplification
-                    $display("Read was interrupted by write.");
+                    $display("We only support INCR write bursts.");
+                    $finish;
+                end
+                if (S_AXI_arvalid && S_AXI_arlen != 0 && S_AXI_arsize != $clog2(BYTES_PER_WORD)) begin
+                    status[2] <= 1;
+                    // This is a simplification
+                    $display("We do not support narrow burst transfers (read).");
+                    $finish;
+                end
+                if (S_AXI_awvalid && S_AXI_awlen != 0 && S_AXI_awsize != $clog2(BYTES_PER_WORD)) begin
+                    status[3] <= 1;
+                    // This is a simplification
+                    $display("We only support narrow burst transfers (write).");
+                    $finish;
+                end
+                if (S_AXI_awvalid && awready && arlen_reg != 0) begin
+                    status[4] <= 1;
+                    $display("Internal error: Read was interrupted by write.");
                     $finish;
                 end
                 if (!write_addr_set && !S_AXI_awvalid && S_AXI_wvalid) begin
-                    status[4] <= 1;
-                    $display("write data should not be there before address.");
-                    $finish;
+                    //status[5] <= 1;
+                    //$display("write data should not be there before address.");
+                    //$finish;
                 end
-                if (write_addr_set && S_AXI_awvalid) begin
-                    status[5] <= 1;
-                    $display("No overlapping writes are supported");
+                if (write_addr_set && S_AXI_awvalid && awready) begin
+                    status[6] <= 1;
+                    $display("Internal error: Accepted an overlapping write, which is not supported");
                     $finish;
-                end else if (S_AXI_awvalid && !S_AXI_wvalid) begin
+                end else if (S_AXI_awvalid && awready && !(S_AXI_wvalid && wready)) begin
                     write_addr_set <= 1;
-                end else if (write_addr_set && S_AXI_wvalid) begin
+                end else if (write_addr_set && S_AXI_wvalid && wready) begin
                     write_addr_set <= 0;
                 end
-                if (S_AXI_wlast && S_AXI_wvalid && wlast_reg) begin
-                    status[6] <= 1;
-                    $display("no overlapping writes are supported, bresp");
+                if (S_AXI_wlast && S_AXI_wvalid && wready && wlast_reg) begin
+                    status[7] <= 1;
+                    $display("Internal error: Accepted two last writes (overlapping?); overlapping writes are not supported");
                     $finish;
                 end
             end
